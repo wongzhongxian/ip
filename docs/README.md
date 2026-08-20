1cc4cc91e2e102a33339da043e3b40588b2e93ac
Wong Zhong Xian <wzhongxianmu@gmail.com>
2026-08-20 13:29:10 +0800

    Rename chatbot from Duke to Clearblue
    Rename the Java entry point, update the ASCII banner, and refresh documentation references.

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Clearblue User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Clearblue.java
@@ -0,0 +1,12 @@
+public class Clearblue {
+    public static void main(String[] args) {
+        String banner = "   ________                __    __         \n"
+                + "  / ____/ /__  ____ ______/ /_  / /_  _____\n"
+                + " / /   / / _ \\/ __ `/ ___/ __ \\/ / / / / _ \\\n"
+                + "/ /___/ /  __/ /_/ / /  / /_/ / / /_/ /  __/\n"
+                + "\\____/_/\\___/\\__,_/_/  /_.___/_/\\__,_/\\___/\n";
+        String introMessage = "Hello! I'm Clearblue. How may I help you today?";
+        System.out.println(banner);
+        System.out.println(introMessage);
+    }
+}
